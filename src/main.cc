631438bb7fcb6124cea7680263d15047b39b5129
Clayton Knittel <cknit1999@gmail.com>
2024-07-25 02:33:35 -0700

    free with size hint

--- src/main.cc
+++ src/main.cc
@@ -21,8 +21,8 @@ int PrintTrace(const std::string& tracefile) {
                   << std::endl;
         break;
       case bench::TraceLine::Op::kCalloc:
-        std::cout << "calloc(" << line->input_size << ") = " << line->result
-                  << std::endl;
+        std::cout << "calloc(" << line->nmemb << ", " << line->input_size
+                  << ") = " << line->result << std::endl;
         break;
       case bench::TraceLine::Op::kRealloc:
         std::cout << "realloc(" << line->input_ptr << ", " << line->input_size
@@ -33,6 +33,12 @@ int PrintTrace(const std::string& tracefile) {
           std::cout << "free(" << line->input_ptr << ")" << std::endl;
         }
         break;
+      case bench::TraceLine::Op::kFreeHint:
+        if (line->input_ptr != nullptr) {
+          std::cout << "free(" << line->input_ptr << ", " << line->input_size
+                    << ")" << std::endl;
+        }
+        break;
     }
   }
 
@@ -40,10 +46,10 @@ int PrintTrace(const std::string& tracefile) {
 }
 
 int main() {
-  // return PrintTrace("traces/simple_realloc.trace");
+  // return PrintTrace("traces/onoro-cc.trace");
 
   absl::Status result =
-      bench::CorrectnessChecker::Check("traces/simple_realloc.trace");
+      bench::CorrectnessChecker::Check("traces/onoro-cc.trace");
   if (!result.ok()) {
     std::cerr << "Failed: " << result << std::endl;
     return -1;
