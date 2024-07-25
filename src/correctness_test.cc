34d398122780c2ff252c00672e9248237e0af634
Clayton Knittel <cknit1999@gmail.com>
2024-07-25 02:42:24 -0700

    correctness tests

---
+++ src/correctness_test.cc
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+
+#include "src/correctness_checker.h"
+#include "src/util.h"
+
+namespace bench {
+
+TEST(TestCorrectness, SimpleRealloc) {
+  ASSERT_THAT(bench::CorrectnessChecker::Check("traces/simple_realloc.trace"),
+              IsOk());
+}
+
+TEST(TestCorrectness, Onoro) {
+  ASSERT_THAT(bench::CorrectnessChecker::Check("traces/onoro.trace"), IsOk());
+}
+
+TEST(TestCorrectness, OnoroCC) {
+  ASSERT_THAT(bench::CorrectnessChecker::Check("traces/onoro-cc.trace"),
+              IsOk());
+}
+
+}  // namespace bench
