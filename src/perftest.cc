3718ec526a3decf27265173432c1d70fe2a20a55
Clayton Knittel <cknit1999@gmail.com>
2024-07-26 15:30:27 -0700

    fix onoro trace and increase heap size

--- src/perftest.cc
+++ src/perftest.cc
@@ -7,6 +7,7 @@
 #include "absl/time/time.h"
 
 #include "src/allocator_interface.h"
+#include "src/fake_heap.h"
 #include "src/tracefile_reader.h"
 #include "src/util.h"
 
@@ -120,6 +121,7 @@ absl::StatusOr<double> TimeTrace(const std::string& tracefile) {
   absl::Time start = absl::Now();
   for (size_t t = 0; t < num_repetitions; t++) {
     FakeHeap::GlobalInstance()->Reset();
+    initialize_heap();
 
     for (const TimeOp& op : ops) {
       switch (op.line.op) {
