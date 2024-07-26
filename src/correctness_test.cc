dd1f00fef43d45ee786f879b0225db9c4ffeaea1
Clayton Knittel <cknit1999@gmail.com>
2024-07-26 01:35:44 -0700

    more traces

--- src/correctness_test.cc
+++ src/correctness_test.cc
@@ -5,6 +5,38 @@
 
 namespace bench {
 
+TEST(TestCorrectness, All) {
+  ASSERT_THAT(bench::CorrectnessChecker::Check("bdd-aa32.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("bdd-aa4.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("bdd-ma4.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("bdd-nq7.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("cbit-abs.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("cbit-parity.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("cbit-satadd.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("cbit-xyz.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("ngram-fox1.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("ngram-gulliver1.trace"),
+              IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("ngram-gulliver2.trace"),
+              IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("ngram-moby1.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("ngram-shake1.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-array.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-array-short.trace"),
+              IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-mix.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-mix-realloc.trace"),
+              IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-mix-short.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-string.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-string-short.trace"),
+              IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-struct.trace"), IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("syn-struct-short.trace"),
+              IsOk());
+  ASSERT_THAT(bench::CorrectnessChecker::Check("test.trace"), IsOk());
+}
+
 TEST(TestCorrectness, Simple) {
   ASSERT_THAT(bench::CorrectnessChecker::Check("traces/simple.trace"), IsOk());
 }
