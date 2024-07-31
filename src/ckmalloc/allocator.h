c1d7dc8f8dfc8ff23c353285b8230ecaa55e252c
Clayton Knittel <cknit1999@gmail.com>
2024-07-31 01:41:09 -0700

    heap interface + allocator test

--- src/ckmalloc/allocator.h
+++ src/ckmalloc/allocator.h
@@ -3,26 +3,25 @@
 #include <cstddef>
 #include <new>
 
-#include "src/fake_heap.h"
+#include "src/heap_interface.h"
 
 namespace ckmalloc {
 
 // Alloc-only allocator that allocates memory directly from the kernel.
-class FakeAllocator {
+class Allocator {
  public:
-  FakeAllocator();
+  // Must be constructed when the heap is empty.
+  explicit Allocator(bench::Heap* heap);
 
   // Allocates `size` bytes aligned to `alignment` and returns a pointer to the
   // beginning of that region.
   //
   // If out of memory, `nullptr` is returned.
-  void* Alloc(size_t size, std::align_val_t alignment);
+  void* Alloc(size_t size,
+              std::align_val_t alignment = static_cast<std::align_val_t>(1));
 
  private:
-  void* region_start_;
-  void* region_end_;
-
-  static constexpr size_t kRegionSize = bench::FakeHeap::kHeapSize;
+  bench::Heap* heap_;
 };
 
 }  // namespace ckmalloc
