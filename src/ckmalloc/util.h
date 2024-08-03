667562dd21167300a355471638221e317457223d
Clayton Knittel <cknit1999@gmail.com>
2024-08-03 12:19:56 -0700

    delete good, now onto delete fix

--- src/ckmalloc/util.h
+++ src/ckmalloc/util.h
@@ -2,12 +2,23 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <optional>
 
 #define CK_ASSERT(cond)                                                     \
   do {                                                                      \
     if (!(cond)) {                                                          \
       std::cerr << __FILE__ ":" << __LINE__ << ": Condition failed: " #cond \
                 << std::endl;                                               \
-      abort();                                                              \
+      std::abort();                                                         \
     }                                                                       \
   } while (0)
+
+namespace ckmalloc {
+
+template <typename T>
+std::optional<T> OptionalOr(std::optional<T>&& primary,
+                            std::optional<T>&& secondary) {
+  return primary.has_value() ? std::move(primary) : std::move(secondary);
+}
+
+}  // namespace ckmalloc
