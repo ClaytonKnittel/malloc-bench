d55c720d73e689e894ee57b3e028f97a46e9b1b9
Clayton Knittel <cknit1999@gmail.com>
2024-08-03 23:03:47 -0700

    clean up delete code

--- src/ckmalloc/red_black_tree.cc
+++ src/ckmalloc/red_black_tree.cc
@@ -1,6 +1,5 @@
 #include "src/ckmalloc/red_black_tree.h"
 
-#include <iomanip>
 #include <optional>
 
 #include "src/ckmalloc/util.h"
@@ -8,46 +7,6 @@
 namespace ckmalloc {
 
 namespace {
-struct Element : public RbNode {
-  int val;
-};
-
-std::ostream& operator<<(std::ostream& ostr, const Element& element) {
-  return ostr << element.val << (element.IsRed() ? " (r)" : " (b)");
-}
-
-bool operator<(const Element& e1, const Element& e2) {
-  return e1.val < e2.val;
-}
-
-void PrintNode(const RbNode* node, int depth) {
-  if (node == nullptr) {
-    return;
-  }
-
-  std::cout << std::setw(2 * depth) << "" << *static_cast<const Element*>(node)
-            << " : ";
-  if (node->Left() != nullptr) {
-    std::cout << "l:" << static_cast<const Element*>(node->Left())->val << " ";
-  }
-  if (node->Right() != nullptr) {
-    std::cout << "r:" << static_cast<const Element*>(node->Right())->val << " ";
-  }
-  if (node->Parent() != nullptr) {
-    std::cout << "p:" << static_cast<const Element*>(node->Parent())->val
-              << " ";
-  }
-  std::cout << std::endl;
-  PrintNode(node->Left(), depth + 1);
-  PrintNode(node->Right(), depth + 1);
-}
-
-void Print(const RbNode* any_node) {
-  while (any_node->Parent() != nullptr) {
-    any_node = any_node->Parent();
-  }
-  PrintNode(any_node, 0);
-}
 
 bool IsRedPtr(const RbNode* node) {
   return node != nullptr && node->IsRed();
@@ -128,16 +87,12 @@ std::optional<RbNode*> RbNode::Remove() const {
     parent = parent_;
     deleted_black = this->IsBlack();
     DetachParent(right_);
-    std::cerr << "Detaching " << static_cast<const Element*>(this)->val
-              << " and replacing with right" << std::endl;
   } else if (right_ == nullptr) {
     replacer = left_;
     successor = left_;
     parent = parent_;
     deleted_black = this->IsBlack();
     DetachParent(left_);
-    std::cerr << "Detaching " << static_cast<const Element*>(this)->val
-              << " and replacing with left" << std::endl;
   } else {
     replacer = left_->RightmostChild();
     successor = replacer->left_;
@@ -153,53 +108,16 @@ std::optional<RbNode*> RbNode::Remove() const {
     replacer->SetRight(right_);
     replacer->SetParentOf(this);
     replacer->red_ = red_;
-
-    std::cerr << "Swapping " << static_cast<const Element*>(this)->val
-              << " with " << static_cast<const Element*>(replacer)->val
-              << std::endl;
   }
-
-  std::cerr << "Tree now:" << std::endl;
-  Print(replacer != nullptr ? replacer : parent != nullptr ? parent : this);
 
   // We have already replaced `this` with `successor`. If `this` was previously
   // the root of the tree, we will need to return the updated root.
   std::optional<RbNode*> new_root =
       parent_ == nullptr ? std::optional<RbNode*>(replacer) : std::nullopt;
   if (!deleted_black) {
-    std::cerr << "deleted red, returning new root: ";
-    if (new_root.has_value()) {
-      if (new_root.value() == nullptr) {
-        std::cerr << "nullptr" << std::endl;
-      } else {
-        std::cerr << static_cast<const Element*>(new_root.value())->val
-                  << std::endl;
-      }
-    } else {
-      std::cerr << "nullopt" << std::endl;
-    }
     return new_root;
   }
 
-  std::cerr << "Fixing starting from ";
-  if (successor != nullptr) {
-    std::cerr << static_cast<const Element*>(successor)->val;
-  } else {
-    std::cerr << "nullptr";
-  }
-  std::cerr << " with parent ";
-  if (parent != nullptr) {
-    std::cerr << static_cast<const Element*>(parent)->val;
-  } else {
-    std::cerr << "nullptr";
-  }
-  std::cerr << std::endl;
-
-  if (new_root.has_value() && new_root.value() != nullptr) {
-    std::cerr << "candidate new root: "
-              << static_cast<const Element*>(new_root.value())->val
-              << std::endl;
-  }
   // If `DeleteFix` returns a new root, return that. Otherwise, return
   // `successor` if `this` was previously the root, else root has not changed.
   return OptionalOr(DeleteFix(successor, parent), std::move(new_root));
@@ -289,7 +207,6 @@ std::optional<RbNode*> RbNode::InsertFix(RbNode* n) {
 
 std::optional<RbNode*> RbNode::DeleteFix(RbNode* n, RbNode* p) {
   if (p == nullptr || IsRedPtr(n)) {
-    std::cerr << "Parent is null or n is red" << std::endl;
     if (IsRedPtr(n)) {
       n->MakeBlack();
     }
@@ -326,21 +243,6 @@ std::optional<RbNode*> RbNode::DeleteFix(RbNode* n, RbNode* p) {
     p = n->Parent();                                  \
     break;                                            \
   }
-
-      std::cerr << "Fixing: n = ";
-      if (n != nullptr) {
-        std::cerr << static_cast<const Element*>(n)->val;
-      } else {
-        std::cerr << "nullptr";
-      }
-      std::cerr << ", p = ";
-      if (p != nullptr) {
-        std::cerr << static_cast<const Element*>(p)->val;
-      } else {
-        std::cerr << "nullptr";
-      }
-      std::cerr << "\n";
-      Print(n != nullptr ? n : p);
 
       if (n == p->Left()) {
         FIX_CHILD(Left, Right);
