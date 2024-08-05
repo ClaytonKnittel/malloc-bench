b1565d075c14a64bee176c4ca509650afcd0103a
Clayton Knittel <cknit1999@gmail.com>
2024-08-05 02:19:22 -0700

    lots of progress

--- src/ckmalloc/slab_id.h
+++ src/ckmalloc/slab_id.h
@@ -5,14 +5,24 @@
 namespace ckmalloc {
 
 class SlabId {
-  friend class SlabManger;
+  friend class SlabManager;
 
  public:
   SlabId(const SlabId& slab_id) = default;
   SlabId& operator=(const SlabId& slab_id) = default;
 
+  SlabId operator+(uint32_t offset) const {
+    return SlabId(slab_idx_ + offset);
+  }
+
+  // The id of the first slab in the heap. This is reserved for the first
+  // metadata slab.
+  static constexpr SlabId Zero() {
+    return SlabId(0);
+  }
+
  private:
-  explicit SlabId(uint32_t slab_idx) : slab_idx_(slab_idx) {}
+  constexpr explicit SlabId(uint32_t slab_idx) : slab_idx_(slab_idx) {}
 
   uint32_t Idx() const {
     return slab_idx_;
