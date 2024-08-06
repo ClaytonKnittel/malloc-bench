c6183e1736cfc103cf72f742c861f873f1a25093
Clayton Knittel <cknit1999@gmail.com>
2024-08-05 19:04:52 -0700

    fix circular dependency, working on slab map

--- src/ckmalloc/state.h
+++ src/ckmalloc/state.h
@@ -6,17 +6,36 @@
 
 namespace ckmalloc {
 
+// Allocates raw memory from the metadata allocator which cannot be freed. This
+// is only intended for metadata allocation, never user data allocation.
+void* MetadataAlloc(size_t size, std::align_val_t alignment);
+
 class State {
  public:
   // Initializes a new `State` with a heap has not been allocated from yet. The
   // `State` takes ownership of the heap.
   static State* InitializeWithEmptyAlloc(bench::Heap* heap);
 
+  // Returns the singleton `State` instance.
+  static State* Instance();
+
+  SlabManager* SlabManager() {
+    return &slab_manager_;
+  }
+
+  MetadataManager* MetadataManager() {
+    return &metadata_manager_;
+  }
+
  private:
   explicit State(bench::Heap* heap);
 
-  SlabManager slab_manager_;
-  MetadataManager metadata_manager_;
+  // This is the global state instance that is initialized with
+  // `InitializeWithEmptyAlloc`.
+  static State* state_;
+
+  class SlabManager slab_manager_;
+  class MetadataManager metadata_manager_;
 };
 
 }  // namespace ckmalloc
