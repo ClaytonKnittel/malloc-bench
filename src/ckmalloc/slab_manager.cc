3975e647510842acb5da994aaaf4e4f8b767d1b4
Clayton Knittel <cknit1999@gmail.com>
2024-08-06 03:01:37 -0700

    slab manager fixup, onto coalesce

--- src/ckmalloc/slab_manager.cc
+++ src/ckmalloc/slab_manager.cc
@@ -3,17 +3,20 @@
 #include <cstddef>
 #include <cstdint>
 #include <optional>
+#include <utility>
 
 #include "src/ckmalloc/common.h"
 #include "src/ckmalloc/free_slab.h"
+#include "src/ckmalloc/slab.h"
 #include "src/ckmalloc/slab_id.h"
+#include "src/ckmalloc/slab_map.h"
 #include "src/ckmalloc/util.h"
 #include "src/heap_interface.h"
 
 namespace ckmalloc {
 
-SlabManager::SlabManager(bench::Heap* heap)
-    : heap_(heap), heap_start_(heap->Start()) {}
+SlabManager::SlabManager(bench::Heap* heap, SlabMap* slab_map)
+    : heap_(heap), heap_start_(heap->Start()), slab_map_(slab_map) {}
 
 void* SlabManager::SlabFromId(SlabId slab_id) const {
   void* slab_start = static_cast<uint8_t*>(heap_start_) +
@@ -30,54 +33,74 @@ SlabId SlabManager::SlabIdFromPtr(void* ptr) const {
   return SlabId(static_cast<uint32_t>(diff / kPageSize));
 }
 
-void* SlabManager::AllocRaw(uint32_t n_pages) {
+std::optional<std::pair<SlabId, Slab*>> SlabManager::Alloc(uint32_t n_pages) {
   if (n_pages == 1 && single_page_freelist_ != nullptr) {
-    FreeSinglePageSlab* slab = single_page_freelist_;
-    single_page_freelist_ = slab->NextFree();
-    return single_page_freelist_;
+    FreeSinglePageSlab* slab_start = single_page_freelist_;
+    single_page_freelist_ = slab_start->NextFree();
+
+    SlabId slab_id = SlabIdFromPtr(slab_start);
+    Slab* slab = slab_map_->FindSlab(slab_id);
+    return std::make_pair(slab_id, slab);
   }
   if (smallest_multi_page_ != nullptr) {
-    FreeMultiPageSlab* slab;
+    FreeMultiPageSlab* slab_start;
     if (n_pages <= 2) {
-      slab = smallest_multi_page_;
+      slab_start = smallest_multi_page_;
     } else {
-      slab = multi_page_free_slabs_.LowerBound(
+      slab_start = multi_page_free_slabs_.LowerBound(
           [n_pages](const FreeMultiPageSlab& slab) {
             return slab.Pages() >= n_pages;
           });
     }
 
-    if (slab != nullptr) {
-      if (slab == smallest_multi_page_) {
-        smallest_multi_page_ = static_cast<FreeMultiPageSlab*>(slab->Next());
+    if (slab_start != nullptr) {
+      if (slab_start == smallest_multi_page_) {
+        smallest_multi_page_ =
+            static_cast<FreeMultiPageSlab*>(slab_start->Next());
       }
-      multi_page_free_slabs_.Remove(slab);
+      multi_page_free_slabs_.Remove(slab_start);
       CK_ASSERT(smallest_multi_page_->Prev() == nullptr);
 
-      CK_ASSERT(slab->Pages() >= n_pages);
-      SlabId slab_id = SlabIdFromPtr(slab);
-      uint32_t actual_pages = slab->Pages();
-      Free(slab_id + n_pages, actual_pages - n_pages);
+      SlabId slab_id = SlabIdFromPtr(slab_start);
+      uint32_t actual_pages = slab_start->Pages();
+      CK_ASSERT(actual_pages >= n_pages);
+      if (actual_pages != n_pages) {
+        // TODO: free can be simpler, check next to see if is free, if so merge
+        // with it. Otherwise do this but no need to coalesce.
+        Slab* remainder = SlabMetadataAlloc();
+        remainder->InitFreeSlab(slab_id + n_pages, actual_pages - n_pages);
+        Free(remainder);
+      }
+
+      Slab* slab = slab_map_->FindSlab(slab_id);
+      return std::make_pair(slab_id, slab);
     }
   }
 
   size_t requested_size = static_cast<size_t>(n_pages) * kPageSize;
-  return heap_->sbrk(requested_size);
+  void* slab_start = heap_->sbrk(requested_size);
+  if (slab_start == nullptr) {
+    return std::nullopt;
+  }
+
+  Slab* slab = SlabMetadataAlloc();
+  if (slab == nullptr) {
+    return std::nullopt;
+  }
+
+  return std::make_pair(SlabIdFromPtr(slab_start), slab);
 }
 
-std::optional<SlabId> SlabManager::Alloc(uint32_t n_pages) {
-  void* slab_start = AllocRaw(n_pages);
-  return slab_start != nullptr ? std::optional(SlabIdFromPtr(slab_start))
-                               : std::nullopt;
-}
-
-void SlabManager::Free(SlabId slab_id, uint32_t n_pages) {
+void SlabManager::Free(Slab* slab) {
+  CK_ASSERT(slab->Type() != SlabType::kFree &&
+            slab->Type() != SlabType::kUnmapped);
+  uint32_t n_pages = slab->Pages();
   if (n_pages == 0) {
     return;
   }
 
-  void* slab_start = SlabFromId(slab_id);
-  // TODO: coalesce (need slab map).
+  void* slab_start = SlabFromId(slab->StartId());
+  // TODO: coalesce.
   if (n_pages == 1) {
     auto* slab = new (slab_start) FreeSinglePageSlab();
     slab->SetNextFree(single_page_freelist_);
--- src/ckmalloc/slab_manager.h
+++ src/ckmalloc/slab_manager.h
@@ -4,7 +4,9 @@
 #include <optional>
 
 #include "src/ckmalloc/free_slab.h"
+#include "src/ckmalloc/slab.h"
 #include "src/ckmalloc/slab_id.h"
+#include "src/ckmalloc/slab_map.h"
 #include "src/heap_interface.h"
 
 namespace ckmalloc {
@@ -13,7 +15,7 @@ using SlabRbTree = RbTree<FreeMultiPageSlab>;
 
 class SlabManager {
  public:
-  explicit SlabManager(bench::Heap* heap);
+  explicit SlabManager(bench::Heap* heap, SlabMap* slab_map);
 
   // Returns a pointer to the start of a slab with given `SlabId`.
   void* SlabFromId(SlabId slab_id) const;
@@ -21,24 +23,27 @@ class SlabManager {
   // Returns the `SlabId` for the slab containing `ptr`.
   SlabId SlabIdFromPtr(void* ptr) const;
 
-  // Allocates `n_pages` contiguous pages, returning a pointer to the start of
-  // the first page in the contiguous block of pages if there is availability,
-  // otherwise returning `nullptr`.
-  void* AllocRaw(uint32_t n_pages);
+  // Allocates `n_pages` contiguous pages, returning the `SlabId` of the first
+  // page in the slab, and an allocated uninitialized `Slab` metadata for this
+  // range of pages, if there was availability, otherwise returning `nullopt`.
+  //
+  // Upon returning, it is the responsibility of the caller to initialize the
+  // returned `Slab`.
+  std::optional<std::pair<SlabId, Slab*>> Alloc(uint32_t n_pages);
 
-  // Allocates `n_pages` contiguous pages, returning the SlabId of the first
-  // page in the contiguous block of pages if there is availability, otherwise
-  // returning `nullopt`.
-  std::optional<SlabId> Alloc(uint32_t n_pages);
-
-  // Frees the slab starting at slab `slab_id` which is `n_pages` long.
-  void Free(SlabId slab_id, uint32_t n_pages);
+  // Frees the slab and takes ownership of the `Slab` metadata object.
+  void Free(Slab* slab);
 
  private:
   // The heap that this SlabManager allocates slabs from.
   bench::Heap* heap_;
   // Cache the heap start from `heap_`, which is guaranteed to never change.
   void* const heap_start_;
+
+  // The slab manager needs to access the slab map when coalescing to know if
+  // the adjacent slabs are free or allocated, and if they are free how large
+  // they are.
+  const SlabMap* slab_map_;
 
   // Single-page slabs are kept in a singly-linked freelist.
   FreeSinglePageSlab* single_page_freelist_ = nullptr;
