2af64e61031b300e971927412467c834c1344435
Paige Knittel <plknit00@gmail.com>
2024-08-06 18:27:22 -0700

    some more small edits

--- src/allocator_interface.h
+++ src/allocator_interface.h
@@ -16,6 +16,30 @@ extern Heap* g_heap;
 
 extern std::mutex g_lock;
 
+size_t static space_needed_with_header(const size_t& size) {
+  // add size of header to size, 8 bytes
+  size_t round_up = size + 0xff;
+  // round up size of memory needed to be 16 byte aligned
+  // zero out first four bits
+  round_up = round_up & ~0xf;
+  return round_up;
+}
+
+size_t static space_needed(const size_t& size) {
+  // round up size of memory needed to be 16 byte aligned
+  // zero out first four bits
+  size_t round_up = size & ~0xf;
+  return round_up;
+}
+
+Block* create_block_extend_heap(size_t size) {
+  auto* block = reinterpret_cast<Block*>(
+      SingletonHeap::GlobalInstance()->sbrk(space_needed_with_header(size)));
+  block->SetBlockSize(size);
+  block->SetFree(false);
+  return block;
+}
+
 // Called before any allocations are made.
 inline void initialize_heap(HeapFactory& heap_factory) {
   auto res = heap_factory.NewInstance(kHeapSize);
