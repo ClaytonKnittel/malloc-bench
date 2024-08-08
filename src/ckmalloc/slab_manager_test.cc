b24fa834017a613c45b5d6a403de08da9f592d55
Clayton Knittel <cknit1999@gmail.com>
2024-08-07 20:46:08 -0700

    lil bit

--- src/ckmalloc/slab_manager_test.cc
+++ src/ckmalloc/slab_manager_test.cc
@@ -2,8 +2,21 @@
 
 #include "gtest/gtest.h"
 
+#include "src/ckmalloc/testlib.h"
+
 namespace ckmalloc {
 
-class SlabManagerTest : public ::testing::Test {};
+class SlabManagerTest : public ::testing::Test {
+ public:
+  // TODO: either make slab_map virtual in slab manager, or template slab map.
+  SlabManagerTest() : heap_(64), slab_manager_(&heap_, &slab_map_) {}
+
+ private:
+  TestHeap heap_;
+  TestSlabMap slab_map_;
+  SlabManager slab_manager_;
+};
+
+TEST_F(SlabManagerTest, TestEmpty) {}
 
 }  // namespace ckmalloc
