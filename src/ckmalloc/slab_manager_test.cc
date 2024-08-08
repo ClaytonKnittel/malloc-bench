26e3be378b400684b9310262bcf380a08afa5308
Paige Knittel <plknit00@gmail.com>
2024-08-08 12:20:51 -0700

    more tests for slab manager

--- src/ckmalloc/slab_manager_test.cc
+++ src/ckmalloc/slab_manager_test.cc
@@ -1,7 +1,10 @@
 #include "src/ckmalloc/slab_manager.h"
 
+#include <cstddef>
+
 #include "gtest/gtest.h"
 
+#include "src/ckmalloc/common.h"
 #include "src/ckmalloc/slab_id.h"
 #include "src/ckmalloc/testlib.h"
 
@@ -32,6 +35,36 @@ class SlabManagerTest : public CkMallocTest {
 TEST_F(SlabManagerTest, HeapStartIsSlabIdZero) {
   SlabManager().Alloc(1);
   EXPECT_EQ(SlabManager().SlabIdFromPtr(Heap().Start()), SlabId(0));
+}
+
+TEST_F(SlabManagerTest, AllPtrsInFirstSlabIdZero) {
+  SlabManager().Alloc(1);
+  for (size_t offset = 0; offset < kPageSize; offset++) {
+    EXPECT_EQ(SlabManager().SlabIdFromPtr(
+                  static_cast<uint8_t*>(Heap().Start()) + offset),
+              SlabId(0));
+  }
+}
+
+TEST_F(SlabManagerTest, SlabIdIncreasesPerPage) {
+  constexpr size_t kPages = 16;
+  SlabManager().Alloc(kPages);
+  for (size_t page_n = 0; page_n < kPages; page_n++) {
+    void* beginning =
+        static_cast<uint8_t*>(Heap().Start()) + page_n * kPageSize;
+    void* end = static_cast<uint8_t*>(beginning) + kPageSize - 1;
+    EXPECT_EQ(SlabManager().SlabIdFromPtr(beginning), SlabId(page_n));
+    EXPECT_EQ(SlabManager().SlabIdFromPtr(end), SlabId(page_n));
+  }
+}
+
+TEST_F(SlabManagerTest, SlabStartFromId) {
+  constexpr size_t kPages = 16;
+  SlabManager().Alloc(kPages);
+  for (size_t page_n = 0; page_n < kPages; page_n++) {
+    EXPECT_EQ(SlabManager().SlabStartFromId(SlabId(page_n)),
+              static_cast<uint8_t*>(Heap().Start()) + page_n * kPageSize);
+  }
 }
 
 }  // namespace ckmalloc
