216d60028b02fd3d41124b9cf22a027d24be2c82
Clayton Knittel <cknit1999@gmail.com>
2024-08-08 20:35:08 -0700

    linked list iterators fixed

---
+++ src/ckmalloc/linked_list_test.cc
@@ -0,0 +1,53 @@
+#include "src/ckmalloc/linked_list.h"
+
+#include <type_traits>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace ckmalloc {
+
+using testing::ElementsAre;
+using testing::Field;
+
+class LinkedListTest : public ::testing::Test {};
+
+struct Item : public LinkedListNode {
+  uint64_t val;
+};
+
+TEST_F(LinkedListTest, TestEmpty) {
+  LinkedList<Item> list;
+  EXPECT_EQ(list.Size(), 0);
+  EXPECT_EQ(list.Front(), nullptr);
+  EXPECT_EQ(list.Back(), nullptr);
+}
+
+TEST_F(LinkedListTest, OneItem) {
+  LinkedList<Item> list;
+  Item item = { .val = 10 };
+  list.InsertFront(&item);
+  EXPECT_EQ(list.Size(), 1);
+  EXPECT_EQ(list.Front(), &item);
+  EXPECT_EQ(list.Back(), &item);
+}
+
+TEST_F(LinkedListTest, InsertFront) {
+  LinkedList<Item> list;
+  Item items[] = {
+    { .val = 10 },
+    { .val = 20 },
+    { .val = 30 },
+  };
+  list.InsertFront(&items[0]);
+  list.InsertFront(&items[1]);
+  list.InsertFront(&items[2]);
+  EXPECT_EQ(list.Size(), 3);
+  EXPECT_EQ(list.Front(), &items[2]);
+  EXPECT_EQ(list.Back(), &items[0]);
+
+  EXPECT_THAT(list, ElementsAre(Field(&Item ::val, 30), Field(&Item ::val, 20),
+                                Field(&Item ::val, 10)));
+}
+
+}  // namespace ckmalloc
