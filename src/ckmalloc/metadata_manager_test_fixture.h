774bfbc1c13f55aba725e983c4b2d1ea2fc6fc23
Clayton Knittel <cknit1999@gmail.com>
2024-08-09 22:32:33 -0700

    metadata manager test now injects behavior in metadatamgr

--- src/ckmalloc/metadata_manager_test_fixture.h
+++ src/ckmalloc/metadata_manager_test_fixture.h
@@ -4,6 +4,8 @@
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 
+#include "src/ckmalloc/metadata_manager.h"
+#include "src/ckmalloc/slab.h"
 #include "src/ckmalloc/slab_manager_test_fixture.h"
 #include "src/ckmalloc/testlib.h"
 #include "src/rng.h"
@@ -14,9 +16,35 @@ class MetadataManagerTest : public SlabManagerTest {
  public:
   static constexpr size_t kNumPages = 64;
 
+  class TestMetadataManager {
+   public:
+    using MetadataManagerT = MetadataManagerImpl<TestSlabMap, TestSlabManager>;
+
+    TestMetadataManager(class MetadataManagerTest* test_fixture,
+                        TestSlabMap* slab_map, TestSlabManager* slab_manager);
+
+    MetadataManagerT& Underlying() {
+      return metadata_manager_;
+    }
+
+    const MetadataManagerT& Underlying() const {
+      return metadata_manager_;
+    }
+
+    void* Alloc(size_t size, size_t alignment = 1);
+
+    Slab* NewSlabMeta();
+
+    void FreeSlabMeta(Slab* slab);
+
+   private:
+    class MetadataManagerTest* test_fixture_;
+    MetadataManagerT metadata_manager_;
+  };
+
   MetadataManagerTest()
       : SlabManagerTest(kNumPages),
-        metadata_manager_(&SlabMap(), &SlabManager()),
+        metadata_manager_(this, &SlabMap(), &SlabManager()),
         rng_(2021, 5) {}
 
   TestMetadataManager& MetadataManager() {
@@ -39,7 +67,14 @@ class MetadataManagerTest : public SlabManagerTest {
 
   // Maps allocations to their sizes and the magic numbers that they are filled
   // with.
-  absl::flat_hash_map<void*, std::pair<size_t, uint64_t>> allocated_blocks_;
+  absl::flat_hash_map<void*, size_t> allocated_blocks_;
+
+  // Maps allocations to the magic numbers that they are filled with. This is
+  // only done for allocations made directly through
+  // `MetadataManagerTest::Alloc`. Other test fixtures which depend on this one
+  // will make allocations through the `TestMetadataManager`, which does not
+  // modify this map.
+  absl::flat_hash_map<void*, uint64_t> block_magics_;
 };
 
 }  // namespace ckmalloc
