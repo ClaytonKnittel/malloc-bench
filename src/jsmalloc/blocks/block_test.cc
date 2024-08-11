f62dcc95076f3f2d05228e2586d153e8c78df3f2
jackson stogel <jtstogel@jtstogel.jacksonstogel>
2024-08-11 11:27:20 -0700

    refactor header bit twiddling to be a little nicer

--- src/jsmalloc/blocks/block_test.cc
+++ src/jsmalloc/blocks/block_test.cc
@@ -6,21 +6,23 @@ namespace jsmalloc {
 namespace blocks {
 
 TEST(TestBlockHeader, Initialization) {
-  BlockHeader header(120, BlockKind::kSmall);
+  BlockHeader header(128, BlockKind::kSmall, false);
 
-  EXPECT_EQ(header.BlockSize(), 120);
+  EXPECT_EQ(header.BlockSize(), 128);
   EXPECT_EQ(header.Kind(), BlockKind::kSmall);
+  EXPECT_EQ(header.PrevBlockIsFree(), false);
 }
 
 TEST(TestBlockHeader, LargeData) {
   uint32_t large_size = (1 << 29) - 1;
-  large_size &= ~0b111;
+  large_size &= ~0b1111;
   BlockKind kind = BlockKind::kBeginOrEnd;
 
-  BlockHeader header(large_size, kind);
+  BlockHeader header(large_size, kind, true);
 
   EXPECT_EQ(header.BlockSize(), large_size);
   EXPECT_EQ(header.Kind(), kind);
+  EXPECT_EQ(header.PrevBlockIsFree(), true);
 }
 
 }  // namespace blocks
--- src/jsmalloc/blocks/free_block_allocator_test.cc
+++ src/jsmalloc/blocks/free_block_allocator_test.cc
@@ -10,36 +10,40 @@
 namespace jsmalloc {
 namespace blocks {
 
+// Minimum size that FreeBlockAllocator will bother saving blocks for.
+constexpr size_t kMinSavedBlockSize = 256;
+
 TEST(TestFreeBlockAllocator, AllocatesExistingBlocks) {
   BigStackAllocator stack_allocator;
   FreeBlockAllocator allocator(stack_allocator);
 
-  FreeBlock* b1 = allocator.Allocate(48);
-  FreeBlock* b2 = allocator.Allocate(64);
-  FreeBlock* b3 = allocator.Allocate(80);
+  FreeBlock* b1 = allocator.Allocate(kMinSavedBlockSize);
+  FreeBlock* b2 = allocator.Allocate(kMinSavedBlockSize + 16);
+  FreeBlock* b3 = allocator.Allocate(kMinSavedBlockSize + 32);
 
   allocator.Free(b1->Header());
-  EXPECT_EQ(b1, allocator.Allocate(48));
+  EXPECT_EQ(b1, allocator.Allocate(kMinSavedBlockSize));
 
   allocator.Free(b2->Header());
-  EXPECT_EQ(b2, allocator.Allocate(64));
+  EXPECT_EQ(b2, allocator.Allocate(kMinSavedBlockSize + 16));
 
   allocator.Free(b3->Header());
-  EXPECT_EQ(b3, allocator.Allocate(80));
+  EXPECT_EQ(b3, allocator.Allocate(kMinSavedBlockSize + 32));
 }
 
 TEST(TestFreeBlockAllocator, SplitsBlocks) {
   BigStackAllocator stack_allocator;
   FreeBlockAllocator allocator(stack_allocator);
 
-  FreeBlock* b = allocator.Allocate(128);
+  FreeBlock* b = allocator.Allocate(kMinSavedBlockSize * 3);
   allocator.Free(b->Header());
 
-  FreeBlock* b1 = allocator.Allocate(48);
-  FreeBlock* b2 = allocator.Allocate(80);
+  FreeBlock* b1 = allocator.Allocate(kMinSavedBlockSize);
+  FreeBlock* b2 = allocator.Allocate(kMinSavedBlockSize);
 
   EXPECT_EQ(b, b1);
-  EXPECT_EQ(reinterpret_cast<uint8_t*>(b) + 48, reinterpret_cast<uint8_t*>(b2));
+  EXPECT_EQ(reinterpret_cast<uint8_t*>(b) + kMinSavedBlockSize,
+            reinterpret_cast<uint8_t*>(b2));
 }
 
 }  // namespace blocks
