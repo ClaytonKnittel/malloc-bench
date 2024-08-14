c8650d553f770c444ddc7d75a50ec5cf3c274d8f
jackson stogel <jtstogel@jtstogel.jacksonstogel>
2024-08-13 22:34:58 -0700

    fix infinite test that doesnt actually use allocated free blocks

--- src/jsmalloc/blocks/free_block_allocator_test.cc
+++ src/jsmalloc/blocks/free_block_allocator_test.cc
@@ -4,6 +4,7 @@
 
 #include "gtest/gtest.h"
 
+#include "src/jsmalloc/blocks/block.h"
 #include "src/jsmalloc/blocks/free_block.h"
 
 namespace jsmalloc {
@@ -12,12 +13,21 @@ namespace blocks {
 // Minimum size that FreeBlockAllocator will bother saving blocks for.
 constexpr size_t kMinSavedBlockSize = 256;
 
+void mark_used(FreeBlock* b) {
+  new (b) BlockHeader(b->BlockSize(), BlockKind::kBeginOrEnd, b->Header()->PrevBlockIsFree());
+}
+
 TEST(TestFreeBlockAllocator, AllocatesExistingBlocks) {
   testing::StackFreeBlockAllocator allocator;
 
   FreeBlock* b1 = allocator.Allocate(kMinSavedBlockSize);
+  mark_used(b1);
+
   FreeBlock* b2 = allocator.Allocate(kMinSavedBlockSize + 16);
+  mark_used(b2);
+
   FreeBlock* b3 = allocator.Allocate(kMinSavedBlockSize + 32);
+  mark_used(b3);
 
   allocator.Free(b1->Header());
   EXPECT_EQ(b1, allocator.Allocate(kMinSavedBlockSize));
@@ -36,7 +46,10 @@ TEST(TestFreeBlockAllocator, SplitsBlocks) {
   allocator.Free(b->Header());
 
   FreeBlock* b1 = allocator.Allocate(kMinSavedBlockSize);
+  mark_used(b1);
+
   FreeBlock* b2 = allocator.Allocate(kMinSavedBlockSize);
+  mark_used(b2);
 
   EXPECT_EQ(b, b1);
   EXPECT_EQ(reinterpret_cast<uint8_t*>(b) + kMinSavedBlockSize,
