e0b7197119f4ec6b6cbc9695ad4eec4b1e43e3b0
Clayton Knittel <cknit1999@gmail.com>
2024-08-22 13:43:34 -0700

    add optional round-up method in utiltest

--- src/utiltest.cc
+++ src/utiltest.cc
@@ -10,6 +10,14 @@
 
 namespace bench {
 
+size_t RoundUp(size_t size) {
+  return size;
+  // if (size <= 8) {
+  //   return 8;
+  // }
+  // return (size + 0xf) & ~0xf;
+}
+
 absl::StatusOr<double> MeasureUtilization(const std::string& tracefile) {
   absl::flat_hash_map<void*, std::pair<void*, size_t>> id_to_ptrs;
   DEFINE_OR_RETURN(TracefileReader, reader, TracefileReader::Open(tracefile));
@@ -28,30 +36,39 @@ absl::StatusOr<double> MeasureUtilization(const std::string& tracefile) {
     switch (line->op) {
       case TraceLine::Op::kMalloc: {
         void* ptr = malloc(line->input_size);
-        id_to_ptrs[line->result] = { ptr, line->input_size };
-        total_allocated_bytes += line->input_size;
+        if (ptr != nullptr) {
+          id_to_ptrs[line->result] = { ptr, line->input_size };
+          total_allocated_bytes += RoundUp(line->input_size);
+        }
         break;
       }
       case TraceLine::Op::kCalloc: {
         void* ptr = calloc(line->nmemb, line->input_size);
-        id_to_ptrs[line->result] = { ptr, line->input_size };
-        total_allocated_bytes += line->input_size;
+        if (ptr != nullptr) {
+          id_to_ptrs[line->result] = { ptr, line->input_size };
+          total_allocated_bytes += RoundUp(line->input_size);
+        }
         break;
       }
       case TraceLine::Op::kRealloc: {
         void* new_ptr =
             realloc(id_to_ptrs[line->input_ptr].first, line->input_size);
         if (line->input_ptr != nullptr) {
-          total_allocated_bytes -= id_to_ptrs[line->input_ptr].second;
+          total_allocated_bytes -= RoundUp(id_to_ptrs[line->input_ptr].second);
           id_to_ptrs.erase(line->input_ptr);
         }
-        total_allocated_bytes += line->input_size;
+        total_allocated_bytes += RoundUp(line->input_size);
         id_to_ptrs[line->result] = { new_ptr, line->input_size };
         break;
       }
       case TraceLine::Op::kFree: {
+        if (line->input_ptr == nullptr) {
+          free(nullptr);
+          break;
+        }
+
         free(id_to_ptrs[line->input_ptr].first);
-        total_allocated_bytes -= id_to_ptrs[line->input_ptr].second;
+        total_allocated_bytes -= RoundUp(id_to_ptrs[line->input_ptr].second);
         id_to_ptrs.erase(line->input_ptr);
         break;
       }
