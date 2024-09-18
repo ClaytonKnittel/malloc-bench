e65189c87656ba24a09a361e7fca0dfd2a04b820
Clayton Knittel <cknit1999@gmail.com>
2024-09-17 18:31:15 -0700

    merge

--- src/perf.cc
+++ src/perf.cc
@@ -8,15 +8,11 @@
 
 int main() {
   for (const auto& tracefile : {
-           "traces/four-in-a-row.trace",
-           "traces/grep.trace",
            "traces/haskell-web-server.trace",
+           "traces/mc_server.trace",
+           "traces/mc_server_large.trace",
+           "traces/mc_server_small.trace",
            "traces/onoro.trace",
-           "traces/onoro-cc.trace",
-           "traces/scp.trace",
-           "traces/solitaire.trace",
-           "traces/ssh.trace",
-           "traces/vim.trace",
        }) {
     bench::MMapHeapFactory heap_factory;
     absl::StatusOr<bench::TracefileReader> reader =
