c969e2f0809c281e2930d39f139aa528c0fda3d7
Clayton Knittel <cknit1999@gmail.com>
2024-09-22 05:51:10 -0700

    more traces in perf

--- src/perf.cc
+++ src/perf.cc
@@ -8,11 +8,21 @@
 
 int main() {
   for (const auto& tracefile : {
+           "traces/firefox.trace",
+           "traces/four-in-a-row.trace",
+           "traces/grep.trace",
            "traces/haskell-web-server.trace",
            "traces/mc_server.trace",
            "traces/mc_server_large.trace",
            "traces/mc_server_small.trace",
            "traces/onoro.trace",
+           "traces/py-catan-ai.trace",
+           "traces/py-euler-nayuki.trace",
+           "traces/scp.trace",
+           "traces/solitaire.trace",
+           "traces/ssh.trace",
+           "traces/vim.trace",
+           "traces/vlc.trace",
        }) {
     bench::MMapHeapFactory heap_factory;
     absl::StatusOr<bench::TracefileReader> reader =
