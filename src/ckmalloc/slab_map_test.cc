e4772df25127fc2f237996d0380c368427ede665
Clayton Knittel <cknit1999@gmail.com>
2024-09-24 14:22:21 -0700

    slab map test large range

--- src/ckmalloc/slab_map_test.cc
+++ src/ckmalloc/slab_map_test.cc
@@ -25,6 +25,8 @@ class SlabMapTest : public testing::Test, public CkMallocTest {
     return absl::OkStatus();
   }
 
+  bool AllocatePath(PageId start_id, PageId end_id);
+
  private:
   TestSlabMap slab_map_;
 };
@@ -81,6 +83,25 @@ TEST_F(SlabMapTest, TestInsertRange) {
 
   constexpr uint64_t kStartIdx = 20 + 5 * kNodeSize;
   constexpr uint64_t kEndIdx = 2 + 15 * kNodeSize;
+  PageId start_id = PageId(kStartIdx);
+  PageId end_id = PageId(kEndIdx);
+  EXPECT_TRUE(SlabMap().AllocatePath(start_id, end_id));
+  SlabMap().InsertRange(start_id, end_id, test_slab);
+
+  for (uint64_t i = kStartIdx - 1000; i <= kEndIdx + 1000; i++) {
+    if (i >= kStartIdx && i <= kEndIdx) {
+      ASSERT_EQ(SlabMap().FindSlab(PageId(i)), test_slab);
+    } else {
+      ASSERT_EQ(SlabMap().FindSlab(PageId(i)), nullptr);
+    }
+  }
+}
+
+TEST_F(SlabMapTest, TestInsertLongRange) {
+  MappedSlab* test_slab = reinterpret_cast<MappedSlab*>(0x123456789abcd0);
+
+  constexpr uint64_t kStartIdx = 20 + 5 * kNodeSize + 3 * kNodeSize * kNodeSize;
+  constexpr uint64_t kEndIdx = 2 + 15 * kNodeSize + 5 * kNodeSize * kNodeSize;
   PageId start_id = PageId(kStartIdx);
   PageId end_id = PageId(kEndIdx);
   EXPECT_TRUE(SlabMap().AllocatePath(start_id, end_id));
