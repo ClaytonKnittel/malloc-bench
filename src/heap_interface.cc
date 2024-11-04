aee058bdb390d16787e6710fb25e52db7d07fc51
Clayton Knittel <cknit1999@gmail.com>
2024-11-03 21:20:29 -0800

    Make heap interface thread-safe

--- src/heap_interface.cc
+++ src/heap_interface.cc
@@ -1,25 +1,28 @@
 #include "src/heap_interface.h"
 
+#include <atomic>
 #include <cerrno>
 
 namespace bench {
 
 void* Heap::sbrk(intptr_t increment) {
-  void* old_heap_end = heap_end_;
-
   if (increment < 0) {
     errno = EINVAL;
     return nullptr;
   }
-  if ((static_cast<uint8_t*>(old_heap_end) -
-       static_cast<uint8_t*>(heap_start_)) +
-          increment >
-      max_size_) {
-    errno = ENOMEM;
-    return nullptr;
-  }
 
-  heap_end_ = static_cast<uint8_t*>(old_heap_end) + increment;
+  void* old_heap_end = heap_end_.load(std::memory_order_relaxed);
+  do {
+    if ((static_cast<uint8_t*>(old_heap_end) -
+         static_cast<uint8_t*>(heap_start_)) +
+            increment >
+        max_size_) {
+      errno = ENOMEM;
+      return nullptr;
+    }
+  } while (!heap_end_.compare_exchange_weak(
+      old_heap_end, static_cast<uint8_t*>(old_heap_end) + increment,
+      std::memory_order_relaxed, std::memory_order_relaxed));
   return old_heap_end;
 }
 
