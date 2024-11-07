fe3342d250b213a8befd111af066ead2d4372a82
ClaytonKnittel <35512940+ClaytonKnittel@users.noreply.github.com>
2024-11-06 23:43:27 -0800

    Perfetto profiling (#75)
    * Perfetto working code.
    
    * Build with perfetto only when enabled.
    
    * Enable perfetto for compile commands.
    
    * Updates.

---
+++ src/perfetto.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifdef PERFETTO_ENABLED
+
+#include "perfetto/tracing/tracing.h"
+#include "perfetto/tracing/track_event.h"
+
+PERFETTO_DEFINE_CATEGORIES(
+    perfetto::Category("test_infrastructure")
+        .SetDescription("Events from testing infrastructure"));
+
+#else
+
+#define TRACE_EVENT(category, name, ...)
+
+#endif
+
+namespace bench {
+
+class Perfetto {
+ public:
+  Perfetto();
+  ~Perfetto();
+
+ private:
+#ifdef PERFETTO_ENABLED
+  std::unique_ptr<perfetto::TracingSession> tracing_session_;
+  int fd_;
+#endif
+};
+
+}  // namespace bench
